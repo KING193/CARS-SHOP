aaa49a644cf189b4cddea1f05c9baa8a45f1eb33
KING193 <123240288+KING193@users.noreply.github.com>
2024-01-02 03:29:37 -1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var player = document.getElementById("player");
+let progress = document.getElementById("progress");
+let playerbtn = document.getElementById("playbtn");
+
+var playpause = function() {
+    if(player.paused) {
+        player.play();
+    } else {
+        player.pause();
+    }
+}
+
+playerbtn.addEventListener("click", playpause);
+
+player.onplay = function() {
+    playerbtn.classList.remove("fa-play")
+    playerbtn.classList.add("fa-pause")
+}
+
+player.onpause = function() {
+    playerbtn.classList.add("fa-play")
+    playerbtn.classList.remove("fa-pause")
+}
+
+player.ontimeupdate = function() {
+    let ct = player.currentTime;
+    current.innerHTML = timeFormat(ct);
+
+    let duration = player.duration;
+    prog = Math.floor((ct*100)/duration)
+    progress.style.setProperty("--progress", prog+"%")
+}
+
+function timeFormat(ct) {
+    minutes = Math.floor(ct/60)
+    seconds = Math.floor(ct%60)
+
+    if(seconds < 10) {
+        seconds = "0" + seconds
+    }
+
+    return minutes+":"+seconds
+}
